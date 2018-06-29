62679f716858a7a58cef99839066a23de8b8b95b
donkeykong91 <darylnator@gmail.com>
2018-06-28 23:26:50 -0700

    Asking questions to the user

--- ask.js
+++ ask.js
@@ -1,3 +1,14 @@
-process.stdout.write("Hello");
+var questions = [
+  "What is your name?",
+  "What is your fav hobby?",
+  "What is your preferred programming language?"
+];
 
-process.stdout.write("World \n\n\n\n");
+var answers = [];
+
+function ask(i) {
+  process.stdout.write("Hello");
+  process.stdout.write("World \n\n\n\n");
+}
+
+ask(0);
